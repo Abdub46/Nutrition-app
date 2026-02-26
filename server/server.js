6c4a5daa274a54e2cf22b42ec402f309ca192947
Abdub46 <abdubgalgallo46@gmail.com>
2026-02-26 15:23:51 +0300

    Server changes

--- server/server.js
+++ server/server.js
@@ -19,6 +19,17 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+
+/******************TEST***********************/
+const sequelize = require("./config/database");
+
+sequelize.authenticate()
+  .then(() => console.log("✅ Database connected successfully"))
+  .catch(err => console.error("❌ Database connection error:", err));
+
+
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
